2138995a52fdd3ace1fed070a2dd6d23230a7de7
jfgiraud <jfgiraud@gmail.com>
2021-10-03 10:55:39 +0200

    Add test

--- tests/validate.sh
+++ tests/validate.sh
@@ -22,11 +22,16 @@ function setup() {
 }
 
 function test_substitution() {
-  actual=$(echo "Hello Jeff" | $ROOTDIR/bin/sandr -s 'Hello' -r 'Bonjour')
-  assert_equals "Bonjour Jeff" "$actual" "bad substitution"
+  actual=$(echo "Hello John DOE" | $ROOTDIR/bin/sandr -s o -r'<o>')
+  assert_equals "Hell<o> J<o>hn DOE" "$actual" "bad substitution"
 }
 
 function test_substitution_ignoring_case() {
-  actual=$(echo "Hello Jeff" | $ROOTDIR/bin/sandr -i -s 'hello' -r 'Bonjour')
-  assert_equals "Bonjour Jeff" "$actual" "bad substitution"
+  actual=$(echo "Hello John DOE" | $ROOTDIR/bin/sandr -i -s o -r'<o>')
+  assert_equals "Hell<o> J<o>hn D<o>E" "$actual" "bad substitution"
+}
+
+function test_reuse_case() {
+  actual=$(echo "Hello John Doe and Jane DOE" | $ROOTDIR/bin/sandr -i -c -s 'doe' -r 'smith')
+  assert_equals "Hello John Smith and Jane SMITH" "$actual" "bad substitution"
 }
